d3a013e9426d0e6d934843b94ed05963b31233e2
suzAnnaKom <kompaktorzsu@gmail.com>
2016-06-02 17:23:51 +0200

    Next

--- inc/js/js.js
+++ inc/js/js.js
@@ -21,7 +21,15 @@ window.onload = function() {
         "4": "#66E1CB",
         "5": "#A675E1"
     };
-    var popup = new ChartPopup("customContextMenu", {
+    var topicBackgroundsL = {
+        "1": "#F9E0EC",
+        "2": "#F9F0E0",
+        "3": "#E4F8E2",
+        "4": "#E0F9F5",
+        "5": "#ede3f9"
+    };
+    var popup = new ChartPopup({
+        containerId: "customContextMenu",
         dataUrl: assetDataURL
     });
 
@@ -37,15 +45,36 @@ window.onload = function() {
     }*/
 
     function nodeStyle(node) {
-        console.log(node);
-        //node.fillColor = null;
-        //node.image = assetImgPath + "faces/" + node.id + ".png";
+
         var label = node.data.label;
         if (node.id.split('.').length > 1) {
             node.display = "text";
             node.fillColor = topicBackgrounds[node.id.split('.').shift()];
+            node.labelStyle.textStyle.font = "12px Open Sans";
             //console.log(node);
+            if (node.selected) {
+                node.labelStyle.textStyle.font = "600 12px Open Sans";
+                node.labelStyle.textStyle.fillColor = "#000";
+                node.lineColor = topicBackgroundsL[node.id.split('.').shift()];
+                node.lineWidth = 2;
+            }
         }
+        else {
+            //console.log(node);
+            node.radius = 40;
+            node.fillColor = null;
+            node.image = assetImgPath + "icons/icon_" + node.id + ".svg";
+            node.labelStyle.textStyle.font = "700 12px  Open Sans";
+
+            if (node == activeNode) {
+                node.fillColor = "#f7f7f7";
+            }
+
+            if (node.selected) {
+                node.fillColor = "#f7f7f7";
+            }
+        }
+
 
         //node.items = [];
        /* if (node == activeNode) {*/
@@ -60,12 +89,11 @@ window.onload = function() {
             if (node.id !== "0") {
                 node.labelStyle.backgroundStyle.fillColor = topicBackgrounds[node.id.split('.').shift()];
             }
-            node.labelStyle.textStyle.font = "12px Open Sans";
+
        /* }*/
 
        if (node.hovered){
-            node.radius = 45;
-            //node.image = "/dvsl/data/net-chart/friend-net/"+node.id+".png";
+            node.radius = node.radius * 1.5;
         }
     }
 
@@ -94,6 +122,7 @@ window.onload = function() {
         },
         navigation: {
             mode: "focusnodes",
+            expandOnClick: false,
             initialNodes: [netChartDefaultValue],
             focusNodeExpansionRadius: 2,
             numberOfFocusNodes: 1,
@@ -104,12 +133,8 @@ window.onload = function() {
         },
         events: {
             onClick: netChartClick,
-            onPositionChange: function(event){
-                if (event.selection.length > 0) {
-                    var node = event.selection[0];
-                    popup.place();
-                }
-            }
+            onPositionChange: netChartPositionChanged,
+            onHoverChange: netCharthoverEvent
         },
         nodeMenu: {enabled: false},
         linkMenu: {enabled: false},
@@ -117,17 +142,19 @@ window.onload = function() {
             fadeTime: 200,
             nodeRadiusExtent: [20, 30],
             node: {
-                cursor: "pointer"
+                cursor: "pointer",
+                display: "image"
             },
             nodeStyleFunction: function(node) {
                 //nodeRadius(node);
                 nodeStyle(node);
             },
             nodeSelected: {
-                cursor: "move"
+                cursor: "move",
+                fillColor: "#f7f7f7"
             },
             nodeFocused: {
-                fillColor: "#afafaf"
+                fillColor: "#f7f7f7"
             },
             nodeLabel: {
                 borderRadius: 5,
@@ -141,13 +168,26 @@ window.onload = function() {
             },
             /*resizing: {enabled: false},*/
             zooming: {
-                /*zoomExtent: [0.2, 3],
-                autoZoomExtent: [0.8, 3],
-                autoZoomSize: 0.9,
+               /* zoomExtent: [0.2, 3],*/
+                autoZoomExtent: [0.6, 1.2]
+                /*autoZoomSize: 0.9,
                 initialAutoZoom: 'false'*/
             }
+        },
+        toolbar: {
+            enabled: true,
+                items:
+                [
+                    { item: "zoomcontrol", side: "left", align: "top" },
+                    { item: "back", side: "top", align: "right" },
+                    { item: "rearrange", side: "top", align: "right" },
+                    { item: "fit", side: "top", align: "right" },
+                    { item: "fullscreen", side: "top", align: "right" }
+                ]
         }
     });
+// var s = netChart.saveState();
+// console.log(s);
     /*var netChart = new NetChart({
         container: document.getElementById('chartDiv'),
         data: {
@@ -238,33 +278,75 @@ window.onload = function() {
         }
     });*/
 
+    function netCharthoverEvent(event){
+
+    }
+
+
+    function netChartPositionChanged(event){
+        if (event.selection.length > 0) {
+            var node = event.selection[0];
+            var n = {id: node.id};
+            $.extend( n, netChart.getNodeDimensions(node) );
+            popup.setOptions({ node: n});
+            //console.log(node.shape.x);
+            //console.log(node.shape.y);
+            //netChart.lockNode(node.id, 0, 0);
+            popup.place();
+            netChart.updateSettings({layout: {mode: "dynamic"}});
+        }
+    };
+
     function netChartClick(event, args) {
         if (!event.ctrlKey && !event.shiftKey && args.clickNode) {
             netChart.addFocusNode(args.clickNode);
            // console.log($('#chartDiv').width());
 
-           // var w = -($('#chartDiv').width() / 2);
-           // var h = -($('#chartDiv').height() / 2);
-           // var x = Math.floor(Math.random() * w);
-           // var y = Math.floor(Math.random() * h);
+            var w = -($('#chartDiv').width() / 2);
+            var h = -($('#chartDiv').height() / 2);
+            var x = 1000/*Math.floor(Math.random() * w)*/;
+            var y = 60/*Math.floor(Math.random() * h)*/;
            // console.log(x);
            // console.log(y);
             //args.clickNode.shape.x = x;
             //args.clickNode.shape.y = y;
             //args.clickNode.shape.hWidth = 730;
-            //args.clickNode.shape.hHeight= 300;
+           // args.clickNode.shape.hHeight= 300;
            // args.clickNode.coordinates = [x,y];
-            //netChart.lockNode(args.clickNode.id, x, y);
+
            // console.log(args.clickNode);
             //netChart.updateStyle([args.clickNode.id]);
+           // console.log($('.DVSL-bar-btn-rearrange').width());
+           //console.log(netChart.getNodeDimensions(args.clickNode));
+           if (args.clickNode.id == "0") {
+            popup.hide();
+             netChart.updateSettings({layout: {nodeSpacing: 4}});
+             //netChart.resetLayout();
+            //$(".DVSL-bar-btn-rearrange").click();
+
+             //netChart.home();
+             //var back = netChart.back();
+             //console.log(back);
+             netChart.resetLayout();
+             netChart.resetLayout();
+           }
+           else {
+            netChart.resetLayout();
+            //netChart.updateSettings({layout: {nodeSpacing: 40}});
+           }
+
+            //netChart.lockNode(args.clickNode, 0, 0);
         }
 
         popup.hide();
         if (args.clickNode) {
             var node = args.clickNode;
-            node.pageX = event.pageX;
+            var n = {id: node.id};
+            $.extend( n, netChart.getNodeDimensions(args.clickNode) );
+            /*node.pageX = event.pageX;
             node.pageY = event.pageY;
-            popup.setNode(node);
+            popup.setNode(node);*/
+            popup.setOptions({ node: n});
             popup.render();
            // menuElement.innerHTML = "Node menu";
         }
@@ -274,15 +356,18 @@ window.onload = function() {
 
         // disable the default context menu
         event.preventDefault();
+        netChart.resetLayout();
     }
 
     function changeTargetNode(node) {
         var nodeId = node.id;
         netChart.updateStyle();
+        netChart.resetLayout();
     }
 
     var updateHeight = function() {
         netChart.updateSettings({area: {height: Math.max(100, window.innerHeight)}});
+        netChart.resetLayout();
     };
     window.onresize = function(event) {
         updateHeight();
@@ -290,28 +375,58 @@ window.onload = function() {
     window.addEventListener('orientationchange', updateHeight);
     updateHeight();
 
+    $(popup.popup).on('shown.popup', function(event) {
+        event.preventDefault();
+        var node = netChart.selection()[0];
+        var dimensions = netChart.getNodeDimensions(node);
+        //netChart.lockNode(node.id, dimensions.x, dimensions.y);
+        // node.fillColor = topicBackgroundsL[node.id.split('.').shift()];
+        // node.lineColor = topicBackgrounds[node.id.split('.').shift()];
+        // node.lineWidth = 1;
+        netChart.updateSettings({layout: {mode: "static", nodeSpacing: 100}});
+        console.log(netChart.selection());
+    });
+
+    $(popup.popup).on('hidden.popup', function(event) {
+        event.preventDefault();
+        //var nodes = netChart.nodes();
+        //var dimensions = netChart.getNodeDimensions(node);
+        //netChart.unLockNode(node.id);
+        netChart.updateSettings({layout: {mode: "dynamic", nodeSpacing: 4}});
+       //console.log(netChart.selection());
+        netChart.resetLayout();
+    });
 
 };
 
-var ChartPopup = function (popupId, options) {
-    this.init(popupId, options);
+var ChartPopup = function (options) {
+    this.init(options);
 };
 
 ChartPopup.prototype = {
+    containerId: null,
     popup: null,
     content: '',
     dataUrl: null,
-    node: null,
+    node: {
+        id: 0,
+        x: 0,
+        y: 0,
+        radius: 0,
+        hwidth: 0
+    },
     $accordion: $( '<div id="accordion"></div>' ),
     $close: $( '<a class="close">×</a>' ),
     siqDashboardPath: '#',
 
-    init: function (popupId, options) {
-        this.popup = document.getElementById(popupId);
+    init: function (options) {
+        this.setOptions(options);
+        this.popup = document.getElementById(this.containerId);
         // the context menu element has to be the direct descendant of the document.body
         document.body.appendChild(this.popup);
         this.dataUrl = options.dataUrl;
-        this.setNode(options.node);
+        // this.events();
+       // this.setNode(options.node);
     },
     render: function () {
         var _self = this;
@@ -349,23 +464,55 @@ ChartPopup.prototype = {
     },
     show: function () {
         this.popup.style.display = "block";
+        $(this.popup).trigger('shown.popup');
     },
     hide: function () {
         this.popup.style.display = "none";
+        $(this.popup).trigger('hidden.popup');
     },
     style: function () {
         this.popup.className = 'popup' + this.node.id.split('.').shift();
     },
-    place: function (x, y) {
-        var x = x || this.node.shape.x + (this.node.shape.hHeight - 17);
-        var y = y || this.node.shape.y + (this.node.shape.hWidth - this.node.shape.hWidth / 2 - 2);
+    place: function (x, y, radius, hwidth) {
+        var x = this.node.x;
+        var y = this.node.y;
+
+        if (this.node.x > ($(window).width() / 2 + 100)) {
+            // Show on the left
+            x = this.node.x - (this.node.hwidth - 20) - $(this.popup).width();
+        }
+        else {
+            // Show on the rigth
+            x = this.node.x + this.node.hwidth - 20;
+        }
+
+        if (this.node.y > ($(window).height() / 2 + 50)) {
+            // Top
+           //console.log(this.node.radius * 2);
+            y = this.node.y - $(this.popup).height() + (this.node.radius);
+        }
+        else {
+            // Bottom
+            y = this.node.y - (this.node.radius);
+        }
+
+        //x = this.node.x - (this.node.hwidth - 20) - $(this.popup).width();
+        //console.log(y);
+        // var x = this.node.x + (this.node.hwidth - 20);
+       //var y = this.node.y + (this.node.radius / 4 - 2);
+       // var x = this.node.x - (this.node.hwidth - 20) - $(this.popup).width();
+        //var y = this.node.y  + (this.node.radius * 2) - $(this.popup).height();
         this.popup.style.left = x + "px";
         this.popup.style.top = y + "px";
         this.popup.style.zIndex = 1300;
     },
-    setNode: function (node) {
-        this.node = node;
+    setOptions: function (options) {
+        $.extend( this, options );
+        return this;
     },
+    /*setNode: function (node) {
+        this.node = node;
+    },*/
     setContent: function (content) {
         var _self = this;
 
@@ -376,6 +523,19 @@ ChartPopup.prototype = {
 
         this.$accordion.accordion({
           heightStyle: "content"
+        });
+
+        this.$accordion.on( "accordionactivate", function( event, ui ) {
+            console.log(event);
+            _self.place();
+        } );
+
+        this.$accordion.find('.figure').each(function(index, el) {
+            console.log($(this));
+            $(this).find('.zoom').on('click', function(event) {
+                event.preventDefault();
+                $(this).closest('.figure').toggleClass('figure-fixed');
+            });
         });
 
         this.$close.on('click', function(event) {
@@ -410,10 +570,22 @@ ChartPopup.prototype = {
         output += '    <input type="hidden" name="format" value="' + boxPath.format + '" />';
         output += '    <input type="hidden" name="path" value="' + boxPath.path + '" />';
         output += '    <input type="hidden" name="box" value="' + boxName + '" />';
-        output += '    <a href="#" onclick="$(this).closest(\'form\').submit(); return false; " >';
-        output += '      <img class="figure" src="' + imagePath + '" alt="' + alt + '" />';
-        output += '     </a>';
+        output += '    <div class="figure">';
+        output += '      <a href="#">';
+        //onclick="$(this).closest(\'form\').submit(); return false; "
+        output += '        <img src="' + imagePath + '" alt="' + alt + '" class="img-responsive" />';
+        output += '        <span class="zoom"><i class="fa" aria-hidden="true"></i></span>';
+        output += '       </a>';
+        output += '     </div>';
         output += '   </form>';
         return output;
     },
+
+    events: function () {
+        var _self= this;
+        this.$accordion.on( "accordionactivate", function( event, ui ) {
+            console.log(event);
+            _self.place();
+        } );
+    }
 };
